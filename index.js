732e9d366d060a1fde9cfb5b8208fb8d1c54d49a
Ishwarya-Kanagaraman <ishwaryaraman324@gmail.com>
2021-07-28 17:00:54 +0530

    connected with mongoDB Atlas

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// import dotenv.config from dotenv
+
 import express from "express";
 const app = express();
 // const router=express.Router();
@@ -9,7 +11,9 @@ const PORT = process.env.PORT || 3001;
 
 // opened connection to db
 
-const url = "mongodb://localhost/Students";
+// const url = "mongodb://localhost/Students";
+const url =
+  "mongodb+srv://ishwarya_23:Kuppu@1614013@cluster0.i7g84.mongodb.net/Students?retryWrites=true&w=majority";
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const con = mongoose.connection;
@@ -18,11 +22,10 @@ con.on("open", () => console.log("MongoDB is connected"));
 // middleware
 app.use(express.json());
 
-app.get('/',async(req,res)=>{
-    res.send("welcome to node app!!!!")
-})
-app.use('/',studentRouter);
-app.use('/',mentorRouter);
-
+app.get("/", async (req, res) => {
+  res.send("welcome to node app!!!!");
+});
+app.use("/", studentRouter);
+app.use("/", mentorRouter);
 
 app.listen(PORT, () => console.log(`server is started at ${PORT}`));
